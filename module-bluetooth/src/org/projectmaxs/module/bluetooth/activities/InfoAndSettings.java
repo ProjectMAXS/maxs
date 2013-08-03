e17e06f139854f29b0a4499762b5a6ef266fd33f
Florian Schmaus <fschmaus@gmail.com>
2013-08-03 14:44:36 +0200

    modules: Activity is now named .activities.InfoAndSettings

--- module-bluetooth/src/org/projectmaxs/module/bluetooth/activities/InfoAndSettings.java
+++ module-bluetooth/src/org/projectmaxs/module/bluetooth/activities/InfoAndSettings.java
@@ -10,6 +10,6 @@ public class InfoAndSettings extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.infoandsettings);
 	}
 }
