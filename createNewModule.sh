6a7b44e04c632de697665c2559bb778afbaf6ffb
Florian Schmaus <fschmaus@gmail.com>
2013-08-09 00:49:58 +0200

    createNewModule.sh recreate Makefile, eclipse and shared links in the source module

--- createNewModule.sh
+++ createNewModule.sh
@@ -26,10 +26,16 @@ cp -r $SRC_MODULE $DEST_MODULE
 rm -rf ${DEST_MODULE}/bin/*
 rm -rf ${DEST_MODULE}/gen/*
 rm -rf ${DEST_MODULE}/Makefile
-mv ${DEST_MODULE}/src/org/projectmaxs/module/${SRC_NAME} ${DEST_MODULE}/src/org/projectmaxs/module/${DEST_NAME} 
+mv ${DEST_MODULE}/src/org/projectmaxs/module/${SRC_NAME} ${DEST_MODULE}/src/org/projectmaxs/module/${DEST_NAME}
+
+# substitute the module name for the new one
 find $DEST_MODULE -type f | xargs sed -i "s/${SRC_NAME}/${DEST_NAME}/"
 
 git add ${DEST_MODULE}
 
 make ${DEST_MODULE}/Makefile
 make -C $DEST_MODULE eclipse
+
+# recreate the Makefile, eclipse links and the shared links in the source module
+make ${SRC_MODULE}/Makefile
+make -C $SRC_MODULE eclipse
