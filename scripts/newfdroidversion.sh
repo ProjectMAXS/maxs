f5545823e3259ada8d84a6c711a6db4839239d08
Florian Schmaus <fschmaus@gmail.com>
2013-10-02 12:39:10 +0200

    Added scripts/

---
+++ scripts/newfdroidversion.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+. setup.sh
+
+if [[ -z $1 ]]; then
+    echo "version not set"
+    exit 1
+fi
+
+
+# Create version entry for the modules
+for m in "${!MOD2PKG[@]}"; do
+    versionCode=$(xml sel -t -v "//manifest/@android:versionCode" ${BASEDIR}/${m}/AndroidManifest.xml)
+    cat <<EOF >> ${FDROIDMETA}/${MOD2PKG[${m}]}.txt
+
+Build Version:${1},${versionCode},${1},\\
+init=(cd .. && make ${m}/Makefile},\\
+subdir=${m},\\
+prebuild=make shared
+EOF
+exit
+done
+
+# Create version entry for main
+versionCode=$(xml sel -t -v "//manifest/@android:versionCode" ${BASEDIR}/main/AndroidManifest.xml)
+cat <<EOF >> ${FDROIDMETA}/org.projectmaxs.main.txt
+
+Build Version:${1},${versionCode},${1},\\
+subdir=main,\\
+prebuild=make resources
+EOF
+
+# Create version entry for transport
+versionCode=$(xml sel -t -v "//manifest/@android:versionCode" ${BASEDIR}/main/AndroidManifest.xml)
+cat <<EOF >> ${FDROIDMETA}/org.projectmaxs.transport.xmpp.txt
+
+Build Version:${1},${versionCode},${1},\\
+subdir=transport-xmpp,\\
+prebuild=make asmack shared
+EOF
---
+++ scripts/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR="$(cd ${SCRIPTDIR}/.. && pwd)"
+
+if [[ ! -f ${BASEDIR}/config ]]; then
+    echo "config not found"
+    exit 1
+fi
+
+. ${BASEDIR}/config
+
+FDROIDMETA="${FDROIDDATA}/metadata"
+MAINDIR="${BASEDIR}/main"
+TRANSPORTS="$(find $BASEDIR -mindepth 1 -maxdepth 1 -type d -name 'transport-*')"
+MODULES="$(find $BASEDIR -mindepth 1 -maxdepth 1 -type d -name 'module-*')"
+
+declare -A MOD2PKG
+
+for m in $MODULES ; do
+    module_name=$(basename $m)
+    module_package=$(xml sel -t -v "//manifest/@package" ${m}/AndroidManifest.xml)
+    MOD2PKG[${module_name}]=${module_package}
+done
