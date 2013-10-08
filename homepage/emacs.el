50a6a9a416af17069eb827880fce191b224990b4
Florian Schmaus <fschmaus@gmail.com>
2013-10-08 20:53:34 +0200

    homepage: updated. make sure that documentation/ is also published

--- homepage/emacs.el
+++ homepage/emacs.el
@@ -6,9 +6,20 @@
 (require 'org-publish)  
 
 (setq org-publish-project-alist
-      '(("maxs"
-	 :base-directory "."
-	 :publishing-directory "/ssh:mate.geekplace.eu:/var/vhosts/projectmaxs.org"
+      '(
+	("documentation"
+	 :base-directory "../documentation"
+	 :publishing-directory "/ssh:mate.geekplace.eu:/var/vhosts/projectmaxs.org/documentation"
 	 :publishing-function org-html-publish-to-html
-	 )))
+	 :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://thomasf.github.io/solarized-css/solarized-light.min.css\" />"
+	 :infojs-opt "view:showall toc:nil ltoc:nil mouse:underline buttons:t path:http://thomasf.github.io/solarized-css/org-info.min.js"
+	 )
+	("maxs"
+	 :base-directory "."
+	 :publishing-directory "/ssh:mate.geekplace.eu:/var/vhosts/projectmaxs.org/homepage"
+	 :publishing-function org-html-publish-to-html
+	 :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://thomasf.github.io/solarized-css/solarized-light.min.css\" />"
+	 :infojs-opt "view:showall toc:nil ltoc:nil mouse:underline buttons:t path:http://thomasf.github.io/solarized-css/org-info.min.js"
+	 )
+	))
 
