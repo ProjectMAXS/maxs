0c41341de17e4d0b4e6957b416bc9b94d0669160
Florian Schmaus <flo@geekplace.eu>
2013-10-22 17:51:20 +0200

    updateAsmack.sh: updated

--- scripts/updateAsmack.sh
+++ scripts/updateAsmack.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-set -x
-set -e
+while getopts dr: OPTION "$@"; do
+    case $OPTION in
+	d)
+	    set -x
+	    ;;
+	r)
+	    RELEASE=${OPTARG}
+	    ;;
+	esac
+done
 
 # config
 ASMACK_RELEASES=http://asmack.freakempire.de/
 
-if [[ $# -ne 1 ]]; then
-    echo "usage: `basename $0` <aSmackRelease>"
+if [[ -z $RELEASE ]]; then
+    echo "usage: `basename $0` [-d] -r <aSmackRelease>"
     exit 1
 fi
 
@@ -21,24 +29,26 @@ OLD_ASMACK_SHA=$(ls build/hashes/asmack-*.sha256)
 MIN_SDK=$(xml sel -t -v "//manifest/uses-sdk/@android:minSdkVersion" AndroidManifest.xml)
 
 TMP_DIR=$(mktemp -d)
+trap "rm -rf ${TMP_DIR}" EXIT
 cd $TMP_DIR
 
-wget ${ASMACK_RELEASES}/${1}/asmack-android-${MIN_SDK}-${1}.jar
-wget ${ASMACK_RELEASES}/${1}/asmack-android-${MIN_SDK}-${1}.jar.sig
+wget ${ASMACK_RELEASES}/${RELEASE}/asmack-android-${MIN_SDK}-${RELEASE}.jar || exit 1
+wget ${ASMACK_RELEASES}/${RELEASE}/asmack-android-${MIN_SDK}-${RELEASE}.jar.sig || exit 1
 
-gpg --verify asmack-android-${MIN_SDK}-${1}.jar.sig
+gpg --verify asmack-android-${MIN_SDK}-${RELEASE}.jar.sig || exit 1
 
-sha256sum asmack-android-${MIN_SDK}-${1}.jar > asmack-android-${MIN_SDK}-${1}.jar.sha256
+sha256sum asmack-android-${MIN_SDK}-${RELEASE}.jar > asmack-android-${MIN_SDK}-${RELEASE}.jar.sha256
 
-mv asmack-android-${MIN_SDK}-${1}.jar.sha256 ${XMPP_TRANSPORT_DIR}/build/hashes
+mv asmack-android-${MIN_SDK}-${RELEASE}.jar.sha256 ${XMPP_TRANSPORT_DIR}/build/hashes
 
 cd "${XMPP_TRANSPORT_DIR}"
-rm -rf $TMP_DIR
 
-git rm $OLD_ASMACK_SHA
-
+# clean the lib dir of asmack
 rm libs/asmack-*
 rm libs-sources/asmack-*
 
-git add build/hashes/asmack-android-${MIN_SDK}-${1}.jar.sha256
+git rm $OLD_ASMACK_SHA
+git add build/hashes/asmack-android-${MIN_SDK}-${RELEASE}.jar.sha256
+git commit -m "Changed aSmack version to ${RELEASE}"
 
+make asmack
