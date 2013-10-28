7903461bece76de82a2b5eedb3cf1ad7718709a1
Florian Schmaus <flo@geekplace.eu>
2013-10-28 20:34:28 +0100

    scripts/setup.sh: Don't fail if config is not found

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,15 +4,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR="$(cd ${SCRIPTDIR}/.. && pwd)"
 HOMEPAGE="${BASEDIR}/homepage"
 DOCDIR="${BASEDIR}/documentation"
-
-if [[ ! -f ${BASEDIR}/config ]]; then
-    echo "config not found"
-    exit 1
-fi
-
-. ${BASEDIR}/config
-
-FDROIDMETA="${FDROIDDATA}/metadata"
 MAINDIR="${BASEDIR}/main"
 TRANSPORTS="$(find $BASEDIR -mindepth 1 -maxdepth 1 -type d -name 'transport-*')"
 MODULES="$(find $BASEDIR -mindepth 1 -maxdepth 1 -type d -name 'module-*')"
@@ -25,3 +16,12 @@ for m in $MODULES ; do
     module_package=$(xml sel -t -v "//manifest/@package" ${m}/AndroidManifest.xml)
     MOD2PKG[${module_name}]=${module_package}
 done
+
+if [[ ! -f ${BASEDIR}/config ]]; then
+    echo "config not found"
+else
+    # config is there, source it
+    . ${BASEDIR}/config
+    # and set further env variables based on the config
+    FDROIDMETA="${FDROIDDATA}/metadata"
+fi
