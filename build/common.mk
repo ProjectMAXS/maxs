d847131882d135cfb68463832daa5d3f59347db7
Florian Schmaus <flo@geekplace.eu>
2014-01-19 19:02:10 +0100

    Use .git/logs/HEAD to determine if version.xml is up-to-date

--- build/common.mk
+++ build/common.mk
@@ -5,9 +5,9 @@ GIT_DIR := $(BASE)
 endif
 
 VERSION_XML := res/values/version.xml
-GIT_LOG_MASTER := $(GIT_DIR)/.git/logs/refs/heads/master
+GIT_LOG_HEAD := $(GIT_DIR)/.git/logs/HEAD
 
-.IGNORE : $(GIT_LOG_MASTER)
+.IGNORE : $(GIT_LOG_HEAD)
 
-res/values/version.xml: $(GIT_LOG_MASTER) AndroidManifest.xml
+res/values/version.xml: $(GIT_LOG_HEAD) AndroidManifest.xml
 	$(BASE)/scripts/createVersionXML.sh -c .
