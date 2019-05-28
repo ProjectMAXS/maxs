ac54fbe02319e2763c5fd8b91266945c39efcb96
Florian Schmaus <flo@geekplace.eu>
2019-05-28 19:57:03 +0200

    scripts: Fix createVersionXML.sh
    The check for the return code has to be done before we invoke 'set'
    which also has a return code.

--- scripts/createVersionXML.sh
+++ scripts/createVersionXML.sh
@@ -30,10 +30,10 @@ if command -v git &> /dev/null && [[ -d ${COMPONENT_DIR}/../.git ]]; then
     GIT_REF=$(git describe --tags --dirty=+)
     set +e
     GIT_SYMBOLIC_REF=$(git symbolic-ref --short HEAD)
-    set -e
     if [[ $? -eq 0 ]]; then
         GIT_REF+="-${GIT_SYMBOLIC_REF}"
     fi
+    set -e
     # Only add the result of git describe if it's not the same string a $VERSION_NAME
     if [[ "$VERSION_NAME" != "$GIT_REF" ]]; then
 		DATE=$(date +%F)
