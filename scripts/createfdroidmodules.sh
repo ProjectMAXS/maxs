5ff7a582b39a1f503e918b9ca31319906965b6fc
Florian Schmaus <flo@geekplace.eu>
2019-05-31 11:58:35 +0200

    createfdroidmodules.sh: Update to reflect latest changes

--- scripts/createfdroidmodules.sh
+++ scripts/createfdroidmodules.sh
@@ -26,36 +26,40 @@ versionName=$(getVersionNameFromManifest ${BASEDIR}/${MODULE}/AndroidManifest.xm
 moduleName=${MODULE#module-}
 
 cat <<EOF > ${FDROIDMETA}/${MOD2PKG[${MODULE}]}.txt
-Categories:System
-License:GPLv3
-Web Site:http://projectmaxs.org
-Source Code:http://projectmaxs.org/source
-Issue Tracker:http://projectmaxs.org/issues
-Donate:http://projectmaxs.org/donate
-Bitcoin:bc1qu482c0tngkcvx3q7mrm8zmuldrh2f2lrh26ym0
+Categories:
+  - System
+License: GPL-3.0-or-later
+Web Site: http://projectmaxs.org
+Source Code: http://projectmaxs.org/source
+Issue Tracker: http://projectmaxs.org/issues
+Donate: http://projectmaxs.org/donate
+Bitcoin: bc1qu482c0tngkcvx3q7mrm8zmuldrh2f2lrh26ym0
 
-Auto Name:MAXS Module ${moduleName}
-Summary:A Module for MAXS
+Auto Name: MAXS Module ${moduleName}
+Summary: A Module for MAXS
 Description:
 This is a Module for MAXS, which does not install any launcher.
 You need "MAXS Main" and a configured MAXS Transport to make use of it.
 .
 
-Repo Type:srclib
-Repo:ProjectMAXS
+Repo Type: srclib
+Repo: ProjectMAXS
 
-Build:${versionName},${versionCode}
-    commit=${versionName}
-    subdir=${MODULE}
-    submodules=yes
-    sudo=apt-get install -y python-gnupg libxml2-utils
-    init=cd .. && \\
-        make ${MODULE}/Makefile
-    prebuild=make prebuild && \
-        rm -rf ../module-shell/libraryProjects/root-commands/ExampleApp
+Builds:
+ - versionName: ${versionName}
+   versionCode: ${versionCode}
+   commit: ${versionName}
+   subdir: ${MODULE}
+   submodules: true
+   sudo: apt-get install -y imagemagick libxml2-utils
+   init: make -C .. ${MODULE}/Makefile
+   gradle:
+     - yes
+   rm:
+     - module-shell/libraryProjects/root-commands/ExampleApp
 
-Auto Update Mode:Version %v
-Update Check Mode:Tags
-Current Version:${versionName}
-Current Version Code:${versionCode}
+Auto Update Mode: Version %v
+Update Check Mode: Tags
+Current Version: ${versionName}
+Current Version Code: ${versionCode}
 EOF
